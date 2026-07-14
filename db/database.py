489f0533a56f42089dd707fb189131d69931f04a
Pratul Kumar <pratulkumar2003@gmail.com>
2026-07-14 22:52:58 +0530

    Add alembic

--- db/database.py
+++ db/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from core.config import settings
 
+print("DATABASE_URL:", repr(settings.DATABASE_URL))
+
 engine = create_engine(
     url=settings.DATABASE_URL,
     echo=True
