6ecb30cdced6d81ecb34bc18d3112da2c363e3de
Pratul Kumar <pratulkumar2003@gmail.com>
2026-07-17 15:50:17 +0530

    Add shortened url generator

---
+++ utils/generator.py
@@ -0,0 +1,9 @@
+import secrets
+import string
+
+BASE62 = string.ascii_letters + string.digits
+
+def generate_code(length:int = 6) -> str:
+    return ''.join(secrets.choice(BASE62) for _ in range(length))
+
+print(generate_code())
