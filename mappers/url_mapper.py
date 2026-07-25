340e7d1f8a2656a07bfb433ed278ba73d78e922d
Pratul Kumar <pratulkumar2003@gmail.com>
2026-07-25 16:34:11 +0530

    Enhance url_mapper.py file(Added custom url in mappers)

--- mappers/url_mapper.py
+++ mappers/url_mapper.py
@@ -1,28 +1,27 @@
 from models import Url
-from core import settings
 from schemas import UrlResponse, UrlInfo
 
 class UrlMapper:
 
     @staticmethod
-    def to_response(url: Url) -> UrlResponse:
+    def to_response(url: Url, base_url:str) -> UrlResponse:
         return UrlResponse(
                             id=str(url.id), 
                             original_url=str(url.original_url), 
                             short_code=url.short_code, 
-                            short_url=f"{settings.BASE_URL}/{url.short_code}",
+                            short_url=f"{base_url}/{url.short_code}",
                             clicks=url.clicks,
                             created_at=url.created_at,
                             expires_at=url.expires_at
                             )
 
     @staticmethod
-    def to_details(url: Url):
+    def to_details(url: Url, base_url:str):
         return UrlInfo(
             id=str(url.id),
             original_url=url.original_url,
             short_code=url.short_code,
-            short_url=f"{settings.BASE_URL}/{url.short_code}",
+            short_url=f"{base_url}/{url.short_code}",
             clicks=url.clicks,
             is_active=url.is_active,
             created_at=url.created_at,
