5825e92f74ec70dcabb0d835822f9147599f672c
Pratul Kumar <pratulkumar2003@gmail.com>
2026-08-09 15:07:38 +0530

    Include Authentication route in router.py

--- api/router.py
+++ api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from .routes import url_router
+from .routes import url_router, auth_router
 
 router = APIRouter()
 
 router.include_router(url_router)
+router.include_router(auth_router)
