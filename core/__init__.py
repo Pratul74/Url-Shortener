ea9ee514bdde29ac571bcaaf7e25186e1c83182e
Pratul Kumar <pratulkumar2003@gmail.com>
2026-08-10 18:55:42 +0530

    feat: Add OAuth dependency

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
 from .config import settings
 from .exception_handlers import register_exception_handlers
 from .security import verify_password, create_access_token, hash_password
+from .dependencies import oauth2_scheme
