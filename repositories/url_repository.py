c65a3588f4255cd2408da593f96f86b41a55dead
Pratul Kumar <pratulkumar2003@gmail.com>
2026-08-12 21:30:27 +0530

    refactor(url-repo): replace get_all with get_all_by_user(user_id)

--- repositories/url_repository.py
+++ repositories/url_repository.py
@@ -9,8 +9,8 @@ class URLRepository(BaseRepository[Url]):
     def __init__(self, db: Session):
         super().__init__(Url, db)
 
-    def get_all(self):
-        return self.db.query(Url).all()
+    def get_all_by_user(self, user_id):
+        return self.db.query(Url).filter(Url.user_id == user_id).all()
 
     def get_by_short_code(self, short_code: str):
         return (
