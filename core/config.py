032602a9c08820368ca1f5f1ce5756307e08d17b
Pratul Kumar <pratulkumar2003@gmail.com>
2026-08-23 21:06:31 +0530

    feat: add redis host and port to settings config

--- core/config.py
+++ core/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REDIS_HOST:str
+    REDIS_PORT:int
 
     model_config=SettingsConfigDict(
         env_file=".env",
