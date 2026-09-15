03cf1526bee7a407ef3574dfe265618143567502
Pratul Kumar <pratulkumar2003@gmail.com>
2026-09-15 23:05:03 +0530

    refactor(db): convert db dependency to async db dependency

--- db/dependencies.py
+++ db/dependencies.py
@@ -1,7 +1,6 @@
 from typing import Annotated
-
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 from .session import get_db
 
-db_dependency = Annotated[Session, Depends(get_db)]
+db_dependency = Annotated[AsyncSession, Depends(get_db)]
