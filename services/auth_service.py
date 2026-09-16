fc5a6413a16d35c44eba7936f166a3fe338ccb55
Pratul Kumar <pratulkumar2003@gmail.com>
2026-09-16 20:31:28 +0530

    refactor(services): convert AuthService to async with AsyncSession
    - Replace Session with AsyncSession
    - Make register and login async, await repo calls
    - Remove manual commit/refresh, now handled in repository layer

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,5 +1,5 @@
 from .base import BaseService
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from mappers import UserMapper
 from schemas import UserCreate, UserResponse, TokenResponse
 from core.security import hash_password, create_access_token, verify_password
@@ -7,31 +7,27 @@ from exceptions import UserAlreadyExistsException, InvalidCredentialsException
 from repositories import UserRepository
 
 class AuthService(BaseService):
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
         super().__init__(db)
         self.repo=UserRepository(db)
 
-    def register(self, request:UserCreate) -> UserResponse:
-        if self.repo.exists_by_email(request.email):
+    async def register(self, request:UserCreate) -> UserResponse:
+        if await self.repo.exists_by_email(request.email):
             raise UserAlreadyExistsException()
 
-        if self.repo.exists_by_username(request.username):
+        if await self.repo.exists_by_username(request.username):
             raise UserAlreadyExistsException()
 
-        user = self.repo.create(
+        user = await self.repo.create(
             username = request.username,
             email = request.email,
             hashed_password = hash_password(request.password)
         )
 
-        self.db.commit()
-
-        self.db.refresh(user)
-
         return UserMapper.to_response(user)
 
-    def login(self, email, password) -> TokenResponse:
-        user = self.repo.get_by_email(email)
+    async def login(self, email, password) -> TokenResponse:
+        user = await self.repo.get_by_email(email)
 
         if not user:
             raise InvalidCredentialsException()
