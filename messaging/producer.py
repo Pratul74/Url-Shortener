4891a5a8647eb27b4417d220f337c652dbbca23a
Pratul Kumar <pratulkumar2003@gmail.com>
2026-09-16 20:53:12 +0530

    feat(messaging): add ClickEventProducer for publishing click events to RabbitMQ
    - Publish ClickEvent messages to configured exchange/routing key
    - Use persistent delivery mode for durability

---
+++ messaging/producer.py
@@ -0,0 +1,21 @@
+from core import settings
+from aio_pika import Message, DeliveryMode
+from .channel import RabbitMQChannel
+from .events import ClickEvent as ClickEventSchema
+
+class ClickEventProducer:
+    async def publish(self, click_event: ClickEventSchema):
+        channel = await RabbitMQChannel.get_channel()
+
+        exchange= await channel.get_exchange(settings.RABBITMQ_EXCHANGE)
+
+        message = Message(
+            body=click_event.model_dump_json().encode(),
+            content_type='application/json',
+            delivery_mode=DeliveryMode.PERSISTENT,
+        )
+
+        await exchange.publish(
+            message,
+            routing_key=settings.RABBITMQ_ROUTING_KEY
+        )
