309cf617d311d222605eb56b65f4b6d9e23d5373
Pratul Kumar <pratulkumar2003@gmail.com>
2026-09-19 14:46:02 +0530

    feat(models): add cascade delete in ClickEvent Model
    - Allowing database to delete all clickevents when a it's url is deleted or removed

--- models/analytics.py
+++ models/analytics.py
@@ -9,7 +9,7 @@ class ClickEvent(Base):
     __tablename__ = "click_events"
 
     id : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,)
-    url_id : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("urls.id"), nullable=False, index=True)
+    url_id : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("urls.id", ondelete="CASCADE"), nullable=False, index=True)
     clicked_at : Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False, index=True)
     ip_address : Mapped[str] = mapped_column(String(45), nullable=False)
     country : Mapped[str] = mapped_column(String(100))
@@ -22,5 +22,5 @@ class ClickEvent(Base):
 
     url: Mapped["Url"] = relationship(
         "Url",
-        back_populates="click_events"
+        back_populates="click_events",
     )
