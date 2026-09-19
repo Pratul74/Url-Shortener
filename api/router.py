8006eae87641f4686b7ca20db090bd826184d571
Pratul Kumar <pratulkumar2003@gmail.com>
2026-09-19 14:02:40 +0530

    feat(api): include analytics router in router.py

--- api/router.py
+++ api/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .routes import url_router, auth_router
+from .routes import url_router, auth_router, analytics_router
 
 router = APIRouter()
 
 router.include_router(url_router)
 router.include_router(auth_router)
+router.include_router(analytics_router)
